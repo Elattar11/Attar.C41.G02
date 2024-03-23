81ce841c6ad940d2f1bdf755a3ffb915dbdce786
Elattar11 <mohamed.ahmed7350@fmail.com>
2024-03-23 23:37:27 +0200

    Part 11 Department Controller - Create

--- Attar.C41.G02.PL/Controllers/DepartmentController.cs
+++ Attar.C41.G02.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,6 @@
 ﻿using Attar.C41.G02.BLL.Interfaces;
 using Attar.C41.G02.BLL.Repositories;
+using Attar.C41.G02.DAL.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Attar.C41.G02.PL.Controllers
@@ -17,6 +18,29 @@ namespace Attar.C41.G02.PL.Controllers
             var departments = _departmentRepo.GetAll();
 
             return View(departments);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public IActionResult Create(Department department)
+        {
+            if (ModelState.IsValid) //Server Side Validation
+            {
+                var count = _departmentRepo.Add(department);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            return View(department);
+
         }
     }
 }
